a24fb7d55434a8823b2c36431dff0f0bae949a98
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2017-01-24 17:48:53 -0600

    Added Minute class

---
+++ app/src/main/java/com/hmelizarraraz/colorweather/Minute.java
@@ -0,0 +1,27 @@
+package com.hmelizarraraz.colorweather;
+
+/**
+ * Created by heriberto on 24/01/17.
+ */
+
+public class Minute {
+
+    private String title;
+    private String rainProbability;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+}
