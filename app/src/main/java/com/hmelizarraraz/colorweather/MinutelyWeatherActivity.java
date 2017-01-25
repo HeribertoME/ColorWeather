ee0006dfc97f96acb098904ff15def26bb65a495
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2017-01-24 23:10:44 -0600

    Se implementa el recyclerView

--- app/src/main/java/com/hmelizarraraz/colorweather/MinutelyWeatherActivity.java
+++ app/src/main/java/com/hmelizarraraz/colorweather/MinutelyWeatherActivity.java
@@ -2,12 +2,47 @@ package com.hmelizarraraz.colorweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.hmelizarraraz.colorweather.Adapters.MinutelyWeatherAdapter;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MinutelyWeatherActivity extends Activity {
+
+    @BindView(R.id.minutelyRecyclerView)
+    RecyclerView minutelyRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minutely_weather);
+        ButterKnife.bind(this);
+
+        ArrayList<Minute> minutes = new ArrayList<>();
+
+        for (int i = 0; i<25; i++) {
+            Minute minute = new Minute();
+
+            minute.setTitle("19.55");
+            minute.setRainProbability(i +"%");
+
+            minutes.add(minute);
+        }
+
+        MinutelyWeatherAdapter minutelyWeatherAdapter = new MinutelyWeatherAdapter(this, minutes);
+
+        minutelyRecyclerView.setAdapter(minutelyWeatherAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        minutelyRecyclerView.setLayoutManager(layoutManager);
+
+        minutelyRecyclerView.setHasFixedSize(true);
+
     }
 }
